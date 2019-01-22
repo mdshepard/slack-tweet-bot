99d0d42a8aac8398a000ba3fe181a9e7c63eeb8d
= <mikaiyl.davis@icloud.com>
2019-01-22 15:58:28 -0500

    [DONE] Added Queue and Thread to TweetBot\n[TODO] Process requests sent via Queue & make CRUD methods for twitter.

--- tweet_bot.py
+++ tweet_bot.py
@@ -1,35 +1,54 @@
 #!/usr/bin/env python3
 __author__ = 'mdshepard', 'mikaiyl', 'tjhindman',
 
-import twitter
+# python-twitter from https://github.com/bear/python-twitter
 import logging
 import logging.config
+import os
+import queue
+import threading
+import twitter
+from twython import Twython
 
-logging.config.fileConfig('logger_config.ini')
-logger = logging.getLogger('twitter')
+# log_config = os.path.join(os.path.dirname(__file__), 'logger.ini')
+# logging.config.fileConfig(log_config, disable_existing_loggers=False)
+# logger = logging.getLogger('twitter')
 
 
-class TweetBot(object):
+class TweetBot(threading.Thread):
 
-    def __init__(self, creds):
+    def __init__(self, creds, queue):
         '''
         Auth should be a dict with twitter API credentials
         that have been imported from a .env or shell environment
         '''
-        if creds:
+        self.creds = creds
+        self.queue = queue
+        if self.creds:
             try:
-                self.api = twitter.Api(**creds)
+                print('trying api')
+                self.api = twitter.Api(creds)
             except Exception as e:
-                logger.error(e)
-        else:
-            logger.warn('No Twitter API Credentials')
+                print(e)
+                # logger.error(e)
+        # else:
+            # logger.warn('No Twitter API Credentials')
+        super().__init__()
 
-    def assign_queue(q):
+    def watch(q):
         '''
         Accept Queue object as argument, interperet subscription info
         and assing to TweetBot object properties
         '''
         pass
+
+    def run(self):
+        while True:
+            try:
+                w = self.queue.get(timeout=2)
+                print(w)
+            except queue.Empty:
+                return
 
 
 if __name__ == "__main__":

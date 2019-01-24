4dceb77cf8199c360279333081db6fe844dcf935
= <mikaiyl.davis@icloud.com>
2019-01-24 16:17:43 -0500

    [BROKEN] Somewhat workable

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
+
+"""
+Main function should tie in Slack API and Twitter API functionality
+to determine which accounts to subscribe to and print some statistics
+(i.e. total uptime, tweets recieved per minute, most frequent tweet
+source, etc.). Should be enabled and disabled, but continue to count traffic.
+"""
+
 __author__ = 'mdshepard', 'mikaiyl', 'tjhindman',
 
 # This needs to be from the python-dotenv package not dotenv
+import argparse
 import dotenv
+import logging
+import logging.config
+import ipdb # noqa
 import queue
-import tweetbot
-import slack
+import signal
+import slackbot
+import sys
+import twitterbot
+
+logging.config.fileConfig("logger.ini")
+logger = logging.getLogger("mainLogger")
+exit_flag = False
 
 
 def get_dotenv():
@@ -15,19 +33,69 @@ def get_dotenv():
     return dotenv.dotenv_values()
 
 
-def main():
-    '''
-    Main function. Everything happens here.
-    '''
+def signal_handler(sig, stack):
+    """Recieves runtime errors to let you know when things are F.U.B.A.R."""
+
+    logger.warn('Received ' + signal.Signals(sig).name)
+
+    global exit_flag
+    exit_flag = True
+
+
+def create_parser():
+    """Creates and returns an argparse cmd line option parser"""
+    parser = argparse.ArgumentParser(description="Perform transformation on input text.") # noqa
+    # parser.add_argument("subs", help="Specifies Twitter handle to be watched by program.") # noqa
+    # parser.add_argument("--channel", help="Specifies Slack channel for notifications to be posted in.") # noqa
+    return parser
+
+
+def main(args):
+    """Implementation of Slack and Twitter modules."""
+
+    logger.info(
+        "----------------------------"
+        "Hal9000 has powered on."
+        "----------------------------")
+
     subs = queue.Queue()
     tweets = queue.Queue()
 
-    twitbot = tweetbot.TweetBot(get_dotenv(), subs, tweets)
-    slackbot = slack.SlackBot(tweets, subs)
+    sbot = slackbot.SlackBot(tweets, subs)
+    sbot.connect_to_stream()
+    sbot.monitor_stream()
+    sbot.start()
 
-    twitbot.start()
-    slackbot.start()
+    tbot = twitterbot.TweetBot(get_dotenv(), subs, tweets)
+    tbot.start()
+
+    while not exit_flag:
+        # ipdb.set_trace()
+        pass
+
+    subs.task_done()
+    tweets.task_done()
+
+    logger.warning("Shutting down... Daisy... Daisy...")
+    logger.info(
+        "----------------------------"
+        "Hal9000 has left the building."
+        "Processed {twitterbot_event_count} events"
+        "in {twitterbot_runtime} minutes."
+        "Detected an average of {twitterbot_tpm} tweets"
+        "per minute."
+        "The most frequent tweet source was {twitterbot_top_src}."
+        "----------------------------"
+    )
+
+    logging.shutdown()
+    raise SystemExit
+    sys.exit()
 
 
 if __name__ == "__main__":
-    main()
+    """This is executed when run from the command line"""
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    main(create_parser().parse_args())
--- twitterbot.py
+++ twitterbot.py
@@ -2,13 +2,14 @@
 __author__ = 'mdshepard', 'mikaiyl', 'tjhindman',
 
 # python-twitter from https://github.com/bear/python-twitter
+import ipdb
 import logging
 import logging.config
 import os
 import queue
 import threading
 import twitter
-from twython import Twython
+from twython import Twython # noqa
 
 log_config = os.path.join(os.path.dirname(__file__), 'logger.ini')
 logging.config.fileConfig(log_config, disable_existing_loggers=False)
@@ -17,17 +18,25 @@ logger = logging.getLogger('twitter')
 
 class TweetBot(threading.Thread):
 
-    def __init__(self, creds, commands, tweets):
+    def __init__(self, creds, from_slack, to_slack):
+        # ipdb.set_trace()
         '''
         Auth should be a dict with twitter API credentials
         that have been imported from a .env or shell environment
         '''
+        print('In tweet bot\n{}'.format(from_slack.get()))
         self.creds = creds
+        self.APP_KEY = self.creds['consumer_key']
+        self.APP_SECRET = self.creds['consumer_secret']
+        self.OAUTH_TOKEN = self.creds['access_token_key']
+        self.OAUTH_TOKEN_SECRET = self.creds['access_token_secret']
         self.commands = commands
         if self.creds:
             try:
                 logger.info('Connecting to Twitter API')
                 self.api = twitter.Api(**creds)
+                self.twy = Twython(self.APP_KEY, self.APP_SECRET)
+                self.stream = ''
             except Exception as e:
                 print(e)
                 # logger.error(e)
